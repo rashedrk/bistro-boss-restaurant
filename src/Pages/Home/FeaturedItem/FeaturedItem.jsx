6a7e53b202d46f4887801e77a768260a1e8e6380
rashedrk <rashedrk001@gmail.com>
2023-05-29 21:47:09 +0600

    parallax implemented

--- src/Pages/Home/FeaturedItem/FeaturedItem.jsx
+++ src/Pages/Home/FeaturedItem/FeaturedItem.jsx
@@ -4,22 +4,24 @@ import './FeaturedItem.css'
 
 const FeaturedItem = () => {
     return (
-        <div className="feature-item text-white">
-            <SectionTitle
-                heading="Featured Item"
-                subHeading="check it out"
-            >
-            </SectionTitle>
+        <div className="feature-item bg-fixed text-white ">
+            <div className="bg-black bg-opacity-60 pt-8 pb-12">
+                <SectionTitle
+                    heading="Featured Item"
+                    subHeading="check it out"
+                >
+                </SectionTitle>
 
-            <div className="flex justify-center items-center px-36 pb-12 pt-10">
-                <div>
-                    <img src={featured} alt="" />
-                </div>
-                <div className="md:ml-10">
-                    <p>March 20, 2023</p>
-                    <p className="uppercase">where can i get some?</p>
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi eveniet inventore ut adipisci quos. Id, suscipit iste! Perspiciatis quibusdam nam voluptatem illo laborum reprehenderit consequuntur quasi! Id earum quasi quam.</p>
-                    <button className="btn btn-outline">Button</button>
+                <div className="flex justify-center items-center px-36 pb-12 pt-10">
+                    <div>
+                        <img src={featured} alt="" />
+                    </div>
+                    <div className="md:ml-10">
+                        <p>March 20, 2023</p>
+                        <p className="uppercase">where can i get some?</p>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi eveniet inventore ut adipisci quos. Id, suscipit iste! Perspiciatis quibusdam nam voluptatem illo laborum reprehenderit consequuntur quasi! Id earum quasi quam.</p>
+                        <button className="btn btn-outline border-0 border-b-2 border-white text-white mt-5">Order Now</button>
+                    </div>
                 </div>
             </div>
         </div>
