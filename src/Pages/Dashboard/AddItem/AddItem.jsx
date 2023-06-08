a8fccd3be16e7f31baa83f6ff4d4118ec0603ed4
rashedrk <rashedrk001@gmail.com>
2023-06-08 14:16:02 +0600

    Save new item to the server

--- src/Pages/Dashboard/AddItem/AddItem.jsx
+++ src/Pages/Dashboard/AddItem/AddItem.jsx
@@ -1,21 +1,39 @@
 import { useForm } from 'react-hook-form';
 import SectionTitle from '../../../Components/SectionTitle/SectionTitle';
+import useAxiosSecure from '../../../hooks/useAxiosSecure';
+import Swal from 'sweetalert2';
 const img_hosting_token = import.meta.env.VITE_IMAGE_HOSTING_TOKEN;
 const AddItem = () => {
     const { register, handleSubmit, reset } = useForm();
     const img_hosting_url = `https://api.imgbb.com/1/upload?expiration=600&key=${img_hosting_token}`;
 
+    const [axiosSecure] = useAxiosSecure();
     const onSubmit = data => {
         const formData = new FormData();
         formData.append('image', data.image[0]);
-        fetch(img_hosting_url,{
+        fetch(img_hosting_url, {
             method: 'POST',
             body: formData
         })
-        .then(res => res.json())
-        .then(imageResponse => {
-            console.log(imageResponse);
-        })
+            .then(res => res.json())
+            .then(imageResponse => {
+                const imgUrl = imageResponse.data.display_url;
+                const { name, category, price, recipe } = data;
+                const newItem = { name, category, price: parseFloat(price), recipe, image: imgUrl }
+                console.log(newItem);
+                axiosSecure.post('/menu', newItem)
+                    .then(data => {
+                        if (data.data.insertedId) {
+                            Swal.fire({
+                                position: 'top-end',
+                                icon: 'success',
+                                title: 'Your item has been saved',
+                                showConfirmButton: false,
+                                timer: 1500
+                            })
+                        }
+                    })
+            })
     }
     return (
         <div>
